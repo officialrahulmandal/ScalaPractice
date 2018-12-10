d08444b738c1a45142c28f01cbcd824058c521ce
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-12-10 20:10:54 +0530

    match expression extended

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -6,7 +6,7 @@ package Demo
 object Demo {
 
   def main(args: Array[String]): Unit = {
-    val age = 18;
+    val age = 100;
 
     age match {
       case 20 => println(age);
@@ -14,6 +14,12 @@ object Demo {
       case 30 => println(age);
       case 40 => println(age);
       case 50 => println(age);
+      case _ => println("default")
+    }
+    val i =7;
+    i match {
+      case 1|3|5|7|9 => println("odd")
+      case 2|4|6|8 => println("even")
     }
 
   }
