b232ecc9f9c3abc1b39f23279981a9a323334ff5
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-12-10 20:12:52 +0530

    functions

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -6,21 +6,11 @@ package Demo
 object Demo {
 
   def main(args: Array[String]): Unit = {
-    val age = 100;
+    def add(x: Int, y: Int): Int = {
+      return x+y;
+    }
 
-    age match {
-      case 20 => println(age);
-      case 18 => println(age);
-      case 30 => println(age);
-      case 40 => println(age);
-      case 50 => println(age);
-      case _ => println("default")
-    }
-    val i =7;
-    i match {
-      case 1|3|5|7|9 => println("odd")
-      case 2|4|6|8 => println("even")
-    }
+    println(add(2,4))
 
   }
 
