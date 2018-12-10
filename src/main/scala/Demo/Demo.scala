d8ee05ea3b165e728ae0ccfccb69a36ac16f8196
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-12-10 20:20:32 +0530

    functions extended

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -6,11 +6,17 @@ package Demo
 object Demo {
 
   def main(args: Array[String]): Unit = {
+    object Math{
+      def sub(x: Int, y: Int): Int = {
+        return x-y;
+      }
+    }
     def add(x: Int, y: Int): Int = {
       return x+y;
     }
 
     println(add(2,4))
+    println(Math.sub(3,2))
 
   }
 
