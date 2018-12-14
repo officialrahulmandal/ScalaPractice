549a61e03ccdad21f0b60f7091c72c8566a507bd
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-12-14 16:04:55 +0530

    Arrays in scala

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -4,23 +4,24 @@ package Demo
   * Created by algoscale on 10/12/18.
   */
 object Demo {
-  val num1 = 75;
-  val num2 = 100.25;
-
-  val str1 : String = "Hello World";
-  val str2 : String = "Max";
-
-
+  var myarray: Array[Int] = new Array[Int](4)
+  val myArray2 = new Array[Boolean](5);
+  val myarray3 = Array(1,2,3,4,5,6,8);
 
   def main(args: Array[String]): Unit = {
-    println(str1.length);
-    println(str1.concat(str2));
-    println(str1 + str2);
+    myarray(0) = 20;
+    myarray(1) = 50;
+    myarray(2) = 10;
+    myarray(3) = 30;
+    println(myarray3.length);
+    val result = myarray ++ myarray3;
+    for (x<- result){
+      println(x);
+    }
+    for(i<-0 to (myarray.length - 1)) {
+      println(myarray(i));
+    }
 
-    val result = printf("(%d -- %f -- %s)",num1,num2,str1);
-    println(result);
-    println("(%d -- %f -- %s)".format(num1,num2,str1));
-    printf("(%d -- %f -- %s)",num1,num2,str1);
     }
 
 
