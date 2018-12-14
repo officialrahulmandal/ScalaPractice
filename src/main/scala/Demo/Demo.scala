77fc9cb5b13b99f867ac7127b2d81e027c89c5df
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-12-14 13:34:13 +0530

    curring extended

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -9,12 +9,17 @@ object Demo {
 
   def add2(x: Int ) = (y: Int) => x+y;
 
+  def add3 (x: Int ) (y: Int) = x + y;
+
 
   def main(args: Array[String]): Unit = {
     println(add(20));
     println(add2(3)(4));
     val sum40 = add2(40);
     println(sum40(100));
+    println(add3(100)(300));
+    val sum50 = add3(50)_;
+    println(sum50(400));
     }
 
 
