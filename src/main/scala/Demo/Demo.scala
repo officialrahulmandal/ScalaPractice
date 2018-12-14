eb0ae10a628fa48bef460228ca54dcd8fea836d3
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2018-12-14 16:53:36 +0530

    Sets in scala

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -4,24 +4,32 @@ package Demo
   * Created by algoscale on 10/12/18.
   */
 object Demo {
-  var myarray: Array[Int] = new Array[Int](4)
-  val myArray2 = new Array[Boolean](5);
-  val myarray3 = Array(1,2,3,4,5,6,8);
+  val mylist: List[Int] = List(1,2,5,8,9,6,4);
+  val names: List[String] =List("Max", "Tom", "John");
 
   def main(args: Array[String]): Unit = {
-    myarray(0) = 20;
-    myarray(1) = 50;
-    myarray(2) = 10;
-    myarray(3) = 30;
-    println(myarray3.length);
-    val result = myarray ++ myarray3;
-    for (x<- result){
-      println(x);
-    }
-    for(i<-0 to (myarray.length - 1)) {
-      println(myarray(i));
+    println(0 :: mylist);
+    println(mylist);
+    println(names);
+    println(1 :: 5 :: 9 :: Nil);
+    println(mylist.head);
+    println(names.tail);
+    println(mylist.tail);
+    println(mylist.isEmpty);
+    println(mylist.reverse);
+    println(List.fill(5)(2));
+    println(mylist.max);
+
+    mylist.foreach( println )
+    var sum : Int = 0;
+    mylist.foreach( sum += _);
+    println(sum);
+
+    for (name <- names) {
+      println(name);
     }
 
+    println(names(0));
     }
 
 
